be7f5bf3c26069631bfa618384669099281612f2
wasalas <wasalas893@gmail.com>
2019-11-14 18:27:32 +0530

    add to activity xml

--- app/src/main/java/com/example/timebase/MainActivity.kt
+++ app/src/main/java/com/example/timebase/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.timebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun increaseScore(view:View){
+        
     }
 }
